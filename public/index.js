547b1158bea4cd1ddf566e6e2a03f0ff1877c7be
ogneyar <ogneyar-ne@ya.ru>
2021-11-12 22:45:14 +0300

    Мелкие правки

--- public/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var	port = 3000;
-
-const app = express()
-
-app.get('*', (req, res) => res.sendFile(__dirname + '/index.html'))
-	
-app.listen(port, () => console.log(`Starting server. http://localhost:${ port }`))
