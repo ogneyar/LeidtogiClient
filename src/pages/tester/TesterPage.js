3394b346a900d17a465210d1b07221f77b4fc73c
ogneyar <ogneyar-ne@ya.ru>
2021-11-25 17:13:33 +0300

    добавил url для каждого товара

--- src/pages/tester/TesterPage.js
+++ src/pages/tester/TesterPage.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button } from 'react-bootstrap'
 import ReactHtmlParser from 'react-html-parser'
 
 import { Alert, Input } from '../../components/myBootstrap'
 import { fetchHusqvarnaGetImage, fetchHusqvarnaGetCharcteristic } from '../../http/paserAPI'
+import { fetchAllProducts, updateProduct } from '../../http/productAPI'
+import translit from '../../utils/translite'
 import InfoPage from '../info/InfoPage'
 
 
@@ -33,6 +35,34 @@ const TesterPage = () => {
         }
     }
 
+    // eslint-disable-next-line
+    const onClickButtonSetUrl = async () => {
+        let response = await fetchAllProducts()
+        // .then(data => {
+        //     setMessage(message + data.map(i => i.id + "\n\r"))
+        //     setShowAlert(true)
+        // })
+
+        // console.log("response",response);
+
+        setShowAlert(true)
+
+        await response.forEach(async i => {
+            if (!i?.url || !i.url.includes(i?.article)) {
+                setTimeout(async() => {
+                    let url = translit(i?.name) + "_" + i?.article
+                    await updateProduct(i?.id,{url})
+                        .then(() => setMessage(message + "id: " + i.id + " url: " + url + "<br /><br />"))
+                },50)
+            }
+        })
+
+        // if (message) setMessage(message + "Закончил!<br /><br />")
+    }
+
+    useEffect(() => {
+    }, [message])
+
     return (
         <InfoPage>
             <div>
@@ -45,6 +75,13 @@ const TesterPage = () => {
                 <Button onClick={onClickButtonGetCharcteristic}>
                     Получить характеристики товара
                 </Button>
+                
+                <hr />
+
+                {/* <Button onClick={onClickButtonSetUrl}>
+                    Задать url товарам
+                </Button> */}
+
             </div>
             <Alert show={showAlert} onHide={() => setShowAlert(false)}>
                 {/* {React.createElement(
