fe85e1610970648b140939266a91a37bbcab7db5
ogneyar <ogneyar-ne@ya.ru>
2021-11-25 17:17:25 +0300

    отложил пока работу с банером

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Top from './Top'
 import NavBar from './NavBar'
 import Aside from './Aside'
+// eslint-disable-next-line
 import Banner from './Banner'
 
 import './Header.css'
@@ -14,7 +15,7 @@ const Header = () => {
         >
             <Top />
             <NavBar />
-            <Banner />
+            {/* <Banner /> */}
             <Aside />
         </header>
     )
