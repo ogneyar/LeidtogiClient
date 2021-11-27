cb55675d3063db6a78852ae152c7c595036dcba6
ogneyar <ogneyar-ne@ya.ru>
2021-11-27 22:19:11 +0300

    перенёс из компонента модальное окно на новую страницу

--- src/components/order/CreateOrder.js
+++ src/components/order/CreateOrder.js
@@ -1,52 +1,23 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { Button, Modal } from 'react-bootstrap'
-import { observer } from 'mobx-react-lite'
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom'
 
-import DeliverySdek from '../delivery/sdek/DeliverySdek'
-import Payment from '../payment/Payment'
-import {Context} from '../..'
-import { Input } from '../myBootstrap'
-import Loading from '../Loading'
-import './CreateOrder.css'
+import { CREATE_ORDER_ROUTE, SCROLL_TOP } from '../../utils/consts'
+import scrollUp from '../../utils/scrollUp'
 
+const CreateOrder = () => {
 
-const CreateOrder = observer((props) => {
-
-    const { user } = useContext(Context)
-
-    const [ choiseDelivery, setСhoiseDelivery ] = useState(true)
-    const [ load, setLoad ] = useState(false)
-    const [ payment, setPayment ] = useState(false)
-    const [ visibleModal, setVisibleModal ] = useState(false)
-    const [ email, setEmail ] = useState("")
-    const [ client, setClient ] = useState("")
-    const [ newEmail, setNewEmail ] = useState("")
-    const [ address, setAddress ] = useState("")
-    const [ deliverySum, setDeliverySum ] = useState("")
-
-    const [ message, setMessage ] = useState("")
-
-    const onHideModal = () => {
-        // let alfaPaymentButton = document.getElementById("alfa-payment-button")
-        // alfaPaymentButton.style.display = "none"
-        setVisibleModal(false)
-    }
-
-    useEffect(() => {
-        if (user?.user?.email) {
-            setEmail(user.user?.email)
-        }
-        if (user?.user?.id) {
-            setClient(user.user.id);
-        }
-    }, [user?.user])
+    const history = useHistory()
 
     return (
         <div
             className="CreateOrder"
         >
             <Button
-                onClick={() => {setVisibleModal(true);setСhoiseDelivery(true)}}
+                onClick={() => {
+                    history.push(CREATE_ORDER_ROUTE)
+                    scrollUp(SCROLL_TOP)
+                }}
                 className="CreateOrderButton"
                 size="lg"
                 variant="success"
@@ -54,88 +25,9 @@ const CreateOrder = observer((props) => {
                 Оформить заказ
             </Button>
 
-            <Modal 
-                show={visibleModal} 
-                onHide={onHideModal}
-                centered
-                aria-labelledby="contained-modal-title-vcenter"
-                size="lg"
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Формирование заказа
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-
-                    {message ? <label style={{color:"red",paddingLeft:"20px"}}>{message}</label> : null}
-
-                    {!email
-                    ?
-                        <div className="CreateOrderInputEmail" >
-                            <label>Нам нужен Ваш Email</label>
-                            <Input value={newEmail} onChange={e => setNewEmail(e.target.value)} />
-                            <span>чтобы мы могли отправить на него чек, как того требует ФЗ №54</span>
-                            <Button size="lg" onClick={()=>{newEmail && setEmail(newEmail)}}>Далее</Button>
-                        </div>
-                    :
-                        choiseDelivery 
-                        ?
-                            load
-                            ? <Loading />
-                            :
-                            <div className="CreateOrderChoiseDelivery" >
-                                <label style={{position:"relative"}}>
-                                    <p style={{position:"absolute",bottom:"-40px",left:"10px"}}>Из города Курск</p>
-                                    {/* <Button size="lg" onClick={()=>{setСhoiseDelivery(false);setPayment(true)}}>Самовывоз</Button> */}
-                                    <Payment 
-                                        text="Самовывоз" 
-                                        variant="success" 
-                                        load={load} 
-                                        setLoad={setLoad} 
-                                        amount={props?.amount} 
-                                        email={email} 
-                                        client={client} 
-                                        setMessage={setMessage}
-                                    />
-                                </label>
-                                <Button size="lg" onClick={()=>{setСhoiseDelivery(false);setPayment(false);setMessage("")}}>С доставкой</Button>
-                            </div>
-                        :
-                            payment
-                            ? 
-                                <div className="CreateOrderPayment" >
-                                    <p>Адрес склада СДЭК: {address}</p>
-                                    <p>Товара на сумму: {props?.amount}</p>
-                                    <p>Доставка на сумму: {deliverySum}</p>
-                                    <p style={{fontSize:"20px"}}>Итого к оплате: {props?.amount + deliverySum}</p>
-
-                                    <Payment 
-                                        address={address}
-                                        deliverySum={deliverySum} 
-                                        // amount={props?.amount} 
-                                        email={email}
-                                        client={client}
-                                        setMessage={setMessage}
-                                    />
-                                </div>
-                            : 
-                                <div className="CreateOrderDeliverySdek" >
-                                    <DeliverySdek 
-                                        setAddress={setAddress} 
-                                        setPayment={setPayment} 
-                                        setDeliverySum={setDeliverySum} 
-                                    />
-                                </div>
-                    }
-
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="outline-danger" onClick={onHideModal}>Закрыть</Button>
-                </Modal.Footer>
-            </Modal>
+            
         </div>
     )
-})
+}
 
 export default CreateOrder
