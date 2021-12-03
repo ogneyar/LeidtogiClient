4d4b2ad88dbdddb003131fac2189838b82f7f372
ogneyar <ogneyar-ne@ya.ru>
2021-12-03 11:20:30 +0300

    страница успешной оплаты для ЯндексДирект коверсии

---
+++ src/pages/congratulation/CongratulationPage.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useQueryParam, NumberParam, StringParam } from 'use-query-params'
+
+import { LK_ROUTE, SUPPORT_ROUTE } from '../../utils/consts'
+import NavLink from '../../components/myBootstrap/NavLink'
+import Loading from '../../components/Loading'
+import InfoPage from '../info/InfoPage'
+
+
+const CongratulationPage = () => {
+    
+    // eslint-disable-next-line
+    const [id, setId] = useQueryParam('id', NumberParam)
+    // eslint-disable-next-line
+    const [email, setEmail] = useQueryParam('email', StringParam)
+
+    if (!id) return <Loading />
+
+    return (
+        <InfoPage>
+            <div className="SuccessPage">
+            
+                <header>Оплата упешно произведена!</header>
+                
+                <p>Номер вашего заказа: <strong>{id}</strong></p>
+
+                <p>Наш менеджер свяжется с Вами в ближайшее время.</p>
+
+                <p>Если Вы зарегистрированный клиент, то посмотреть информацию о Ваших заказах можете в <NavLink to={LK_ROUTE}>личном кабинете</NavLink>, а все возникшие вопросы направляйте в <NavLink to={SUPPORT_ROUTE}>тех. поддержку.</NavLink></p>
+
+                <p>Если Вы НЕ зарегистрированный клиент, тогда все возникающие вопросы направляйте на email адрес - <a href="mailto:it@leidtogi.ru">it@leidtogi.ru</a></p>
+                
+                <p>Чек с информацией о Вашем заказе будет отправлен на указанный email{email && ` (${email})`}.</p>
+
+            </div>
+        </InfoPage>
+    )
+}
+
+export default CongratulationPage
