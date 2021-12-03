3c627a8a7eab85da242a33c70552305584f694b1
ogneyar <ogneyar-ne@ya.ru>
2021-12-03 11:22:22 +0300

    страница перехода к оплате для ЯндексМетрики

---
+++ src/pages/pay/PayPage.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+// eslint-disable-next-line
+import { useQueryParam, NumberParam, StringParam } from 'use-query-params'
+
+import InfoPage from '../info/InfoPage'
+import Loading from '../../components/Loading'
+import './PayPage.css'
+
+
+const PayPage = (props) => {
+    
+    // eslint-disable-next-line
+    const [redirectUrl, setRedirectUrl] = useQueryParam('redirect_url', StringParam)
+   
+    useEffect(() => {
+        if (redirectUrl) window.open(redirectUrl,'_self',false)
+    },[redirectUrl])
+
+
+    return (
+        <InfoPage>
+            <div className="PayPage">
+                <div>
+                    <p>Ожидайте...</p>
+                </div>
+                <div>
+                    <Loading />
+                </div>
+            </div>
+        </InfoPage>
+    )
+}
+
+export default PayPage
