e6e6e6ab4e72f6626d5d62099ec71fb321230609
ogneyar <ogneyar-ne@ya.ru>
2021-12-11 19:20:07 +0300

    добавил кнопки для показа товаров с отсутствующими параметрами

--- src/pages/tester/TesterPage.js
+++ src/pages/tester/TesterPage.js
@@ -1,15 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { Button } from 'react-bootstrap'
 import ReactHtmlParser from 'react-html-parser'
 
 import { Alert, Input } from '../../components/myBootstrap'
 import { fetchHusqvarnaGetImage, fetchHusqvarnaGetCharcteristic } from '../../http/paserAPI'
-import { fetchAllProducts, updateProduct } from '../../http/productAPI'
+// eslint-disable-next-line
+import { fetchAllProducts, updateProduct, fetchProductSizes } from '../../http/productAPI'
+import { getAllProductSizes } from '../../http/productSizeAPI'
+import { getAllProductInfos } from '../../http/productInfoAPI'
 import translit from '../../utils/translite'
 import InfoPage from '../info/InfoPage'
+import { Context } from '../..'
 
 
 const TesterPage = () => {
+
+    const { product } = useContext(Context)
 
     const [ showAlert, setShowAlert ] = useState(false)
     const [ message, setMessage ] = useState("")
@@ -60,12 +66,104 @@ const TesterPage = () => {
         // if (message) setMessage(message + "Закончил!<br /><br />")
     }
 
+
     useEffect(() => {
     }, [message])
+
+    const getProductWithOutGabarites = async () => {
+        let sizes = await getAllProductSizes()
+        setShowAlert(true)
+        let messageInfo = product
+            .allProducts
+            .filter(i => {
+                let no = true
+                sizes.forEach(j => {
+                    if (i.id === j.productId) no = false
+                })
+                return no
+            })
+            .map(j => j.id)
+            .join(" ")
+        if (!messageInfo) messageInfo = "Нет таких."
+        setMessage(messageInfo)
+    }
+
+    const getProductWithOutDescription = async () => {
+        let infos = await getAllProductInfos()
+        setShowAlert(true)
+        let messageInfo = product
+            .allProducts
+            .filter(i => {
+                let no = true
+                infos.forEach(j => {
+                    if (i.id === j.productId && j.title === "description") no = false
+                })
+                return no
+            })
+            .map(j => j.id)
+            .join(" ")
+        if (!messageInfo) messageInfo = "Нет таких."
+        setMessage(messageInfo)
+    }
+
+    const getProductWithOutCharacteristics = async () => {
+        let infos = await getAllProductInfos()
+        setShowAlert(true)
+        let messageInfo = product
+            .allProducts
+            .filter(i => {
+                let no = true
+                infos.forEach(j => {
+                    if (i.id === j.productId && j.title === "characteristics") no = false
+                })
+                return no
+            })
+            .map(j => j.id)
+            .join(" ")
+        if (!messageInfo) messageInfo = "Нет таких."
+        setMessage(messageInfo)
+    }
+
+    const getProductWithOutEquipment = async () => {
+        let infos = await getAllProductInfos()
+        setShowAlert(true)
+        let messageInfo = product
+            .allProducts
+            .filter(i => {
+                let no = true
+                infos.forEach(j => {
+                    if (i.id === j.productId && j.title === "equipment") no = false
+                })
+                return no
+            })
+            .map(j => j.id)
+            .join(" ")
+        if (!messageInfo) messageInfo = "Нет таких."
+        setMessage(messageInfo)
+    }
+
 
     return (
         <InfoPage>
             <div>
+                <hr />
+                <Button onClick={getProductWithOutGabarites}>
+                    Поcмотреть товары без габаритов
+                </Button>
+                <hr />
+                <Button onClick={getProductWithOutDescription}>
+                    Поcмотреть товары без описания
+                </Button>
+                <hr />
+                <Button onClick={getProductWithOutCharacteristics}>
+                    Поcмотреть товары без характеристик
+                </Button>
+                <hr />
+                <Button onClick={getProductWithOutEquipment}>
+                    Поcмотреть товары без комплектации
+                </Button>
+                <hr />
+                <br />
                 <Input type="text" value={article} onChange={(e) => setArticle(e.target.value)} />
                 <hr />
                 <Button onClick={onClickButtonGetImage}>
