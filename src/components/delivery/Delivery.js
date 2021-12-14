ed7f9d29ec26c9c19c8efa6c0645d8990badb2b0
ogneyar <ogneyar-ne@ya.ru>
2021-12-14 17:13:38 +0300

    перенёс карту, сделал её общей для всех доставщиков

--- src/components/delivery/Delivery.js
+++ src/components/delivery/Delivery.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import ReactHtmlParser from 'react-html-parser'
+// eslint-disable-next-line
+import { YMaps, Map, Placemark, FullscreenControl, TypeSelector, ZoomControl } from 'react-yandex-maps'
+
+import DeliveryBusinessLines from './DeliveryBusinessLines'
+import DeliveryBoxberry from './boxberry/DeliveryBoxberry'
+import { SCROLL_TOP } from '../../utils/consts'
 import DeliverySdek from './sdek/DeliverySdek'
 import DeliveryPochta from './DeliveryPochta'
-import DeliveryBusinessLines from './DeliveryBusinessLines'
+import scrollUp from '../../utils/scrollUp'
 import DeliveryPek from './DeliveryPek'
-import DeliveryBoxberry from './DeliveryBoxberry'
-
+import { Alert } from '../myBootstrap'
+import Loading from '../Loading'
 import './Delivery.css'
 
 
@@ -12,18 +19,62 @@ export default function Delivery(props) {
 
     const [delivery, setDelivery] = useState("sdek")
 
+    const [ latitude, setLatitude ] = useState(55.75) // Долгота Белой Калитвы - 48.177645
+    const [ longitude, setLongitude ] = useState(37.615) // Широта Белой Калитвы - 48.177645
+
+    const [alertVisible, setAlertVisible] = useState(false)
+    const [textAlert, setTextAlert] = useState("")
+    
+    const [ placemark, setPlacemark ] = useState([]) //
+
+    const [ smallWidth, setSmallWidth ] = useState(window.innerWidth < 1000) //
+    // eslint-disable-next-line
+    const [ loading, setLoading ] = useState(true) //
+
+    
+    useEffect(() => {
+        if (textAlert) {
+            setAlertVisible(true)
+        }
+    },[textAlert])
+    
+    const resize = () => {
+        let div = document.getElementById("DeliveryComponent")
+        if (div) {
+            if (window.innerWidth < 1000) {
+                div.style.width = window.innerWidth - 100 + "px"
+                setSmallWidth(true)
+            }else {
+                div.style.width = "100%"
+                setSmallWidth(false)
+            }
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', resize)
+        
+        if (document.readyState === 'complete' || document.readyState === 'interactive') setLoading(false)
+    },[])
+
+    
+    // if (document.readyState === 'loading') return <Loading />
+    if (loading) return <Loading />
+
+
     return (
         <div 
-            className="Delivery"
+            className="DeliveryComponent"
+            id="DeliveryComponent"
         >
 
             <header className="DeliveryHeader">
                 <h4>Расчёт доставки&nbsp;
                     {delivery === "sdek" && `"Службой Доставки Экспресс Курьер"`}
+                    {delivery === "boxberry" && `"Boxberry"`}
                     {delivery === "pochta" && `"Почтой России"`}
                     {delivery === "businessLines" && `"Деловыми Линиями"`}
                     {delivery === "pek" && `"Первой Экспедиционной Компанией"`}
-                    {delivery === "boxberry" && `"Боксберри"`}
                 </h4>
             </header>
 
@@ -33,38 +84,54 @@ export default function Delivery(props) {
                 <button
                     className={delivery === "sdek" ? "DeliveryActionButton" : "DeliveryButton"}
                     disabled={delivery === "sdek"}
-                    onClick={() => setDelivery("sdek")}
+                    onClick={() => {
+                        setDelivery("sdek")
+                        setPlacemark([])
+                    }}
                 >
                     СДЭК
                 </button>
                 <button
-                    className={delivery === "pochta"  ? "DeliveryActionButton" : "DeliveryButton"}
-                    disabled={delivery === "pochta"}
-                    onClick={() => setDelivery("pochta")}
-                >
-                    Почта России
-                </button>
-                <button
-                    className={delivery === "businessLines" ? "DeliveryActionButton" : "DeliveryButton"}
-                    disabled={delivery === "businessLines"}
-                    onClick={() => setDelivery("businessLines")}
-                >
-                    Деловые Линии
-                </button>
-                <button
-                    className={delivery === "pek" ? "DeliveryActionButton" : "DeliveryButton"}
-                    disabled={delivery === "pek"}
-                    onClick={() => setDelivery("pek")}
-                >
-                    ПЭК
-                </button>
-                <button
                     className={delivery === "boxberry" ? "DeliveryActionButton" : "DeliveryButton"}
                     disabled={delivery === "boxberry"}
-                    onClick={() => setDelivery("boxberry")}
+                    onClick={() => {
+                        setDelivery("boxberry")
+                        setPlacemark([])
+                    }}
                 >
-                    Боксберри
+                    Boxberry
                 </button>
+                {/* <button
+                    className={delivery === "pochta"  ? "DeliveryActionButton" : "DeliveryButton"}
+                    disabled={delivery === "pochta"}
+                    onClick={() => {
+                        setDelivery("pochta")
+                        setPlacemark([])
+                    }}
+                >
+                    Почта России
+                </button> */}
+                {/* <button
+                    className={delivery === "businessLines" ? "DeliveryActionButton" : "DeliveryButton"}
+                    disabled={delivery === "businessLines"}
+                    onClick={() => {
+                        setDelivery("businessLines")
+                        setPlacemark([])
+                    }}
+                >
+                    Деловые Линии
+                </button> */}
+                {/* <button
+                    className={delivery === "pek" ? "DeliveryActionButton" : "DeliveryButton"}
+                    disabled={delivery === "pek"}
+                    onClick={() => {
+                        setDelivery("pek")
+                        setPlacemark([])
+                    }}
+                >
+                    ПЭК
+                </button> */}
+                
             </div>
 
             <div
@@ -72,28 +139,120 @@ export default function Delivery(props) {
             >
                 {delivery === "sdek" 
                 ? 
-                    <DeliverySdek />
-                : null}
-
-                {delivery === "pochta" 
-                ? 
-                    <DeliveryPochta />
-                : null}
-
-                {delivery === "businessLines" 
-                ? 
-                    <DeliveryBusinessLines />
-                : null}
-
-                {delivery === "pek" 
-                ? 
-                    <DeliveryPek />
+                    <DeliverySdek 
+                        {...props} 
+                        setLatitude={setLatitude} 
+                        setLongitude={setLongitude} 
+                        setTextAlert={setTextAlert} 
+                        setPlacemark={setPlacemark}
+                        setLoadingDelivery={setLoading}
+                    />
                 : null}
 
                 {delivery === "boxberry" 
                 ? 
-                    <DeliveryBoxberry />
+                    <DeliveryBoxberry 
+                        {...props} 
+                        setLatitude={setLatitude} 
+                        setLongitude={setLongitude} 
+                        setTextAlert={setTextAlert} 
+                        setPlacemark={setPlacemark}
+                        setLoadingDelivery={setLoading}
+                    />
                 : null}
+
+                {delivery === "pochta" 
+                ? 
+                    <DeliveryPochta 
+                        {...props} 
+                        setLatitude={setLatitude} 
+                        setLongitude={setLongitude} 
+                        setTextAlert={setTextAlert} 
+                        setPlacemark={setPlacemark}
+                        setLoadingDelivery={setLoading}
+                    />
+                : null}
+
+                {delivery === "businessLines" 
+                ? 
+                    <DeliveryBusinessLines 
+                        {...props} 
+                        setLatitude={setLatitude} 
+                        setLongitude={setLongitude} 
+                        setTextAlert={setTextAlert} 
+                        setPlacemark={setPlacemark}
+                        setLoadingDelivery={setLoading}
+                    />
+                : null}
+
+                {delivery === "pek" 
+                ? 
+                    <DeliveryPek 
+                        {...props} 
+                        setLatitude={setLatitude} 
+                        setLongitude={setLongitude} 
+                        setTextAlert={setTextAlert} 
+                        setPlacemark={setPlacemark}
+                        setLoadingDelivery={setLoading}
+                    />
+                : null}
+                
+            </div>
+
+            <div id="YMaps">
+            <YMaps>
+                <Map
+                    state={{ 
+                        // Широта (latitude), Долгота (longitude)
+                        // center: [55.75, 37.615], // Москва
+                        // center: [48.177645, 40.802384], // Белая Калитва
+                        center: [latitude, longitude], 
+                        type: 'yandex#map',
+                        zoom: 12,
+                        controls: [],
+                    }} 
+                    width="100%" 
+                    height={smallWidth ? "600px" : "400px"} 
+                >
+                
+                    {placemark && Array.isArray(placemark) && placemark[0]?.latitude !== undefined
+                    ?
+                        placemark.map(i => 
+                            <Placemark 
+                                key={i?.latitude + i?.longitude}
+                                geometry={[i?.latitude, i?.longitude]} 
+                                // options={{
+                                //     iconLayout: 'default#image',
+                                //     iconImageHref: 'images/delivery/sdek/sdek.png',
+                                //     iconImageSize: [40, 40],
+                                //   }}
+                                onClick={async()=> {
+                                    if (props?.setAddress) props?.setAddress(i?.address)
+                                    setLoading(true)
+                                    scrollUp(SCROLL_TOP)
+                                    i?.onClick()
+                                }}
+                                
+                            />
+                        )
+                    :null}
+                    
+                    {/* <FullscreenControl /> */}
+                    <TypeSelector options={{ float: 'right' }} />
+                    {/* <ZoomControl options={{ float: 'right' }} /> */}
+                
+                </Map>
+            </YMaps>
+
+            <Alert 
+                show={alertVisible} 
+                onHide={() => {
+                    setAlertVisible(false)
+                    setTextAlert("")
+                }}
+            >
+                {ReactHtmlParser(textAlert)}
+            </Alert> 
             </div>
 
         </div>
