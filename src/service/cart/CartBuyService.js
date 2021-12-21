a5cd45f17e298b705f9702f1643a3c4faf7b6426
ogneyar <ogneyar-ne@ya.ru>
2021-12-21 16:21:27 +0300

    добавил возможность уменьшения количества и удаления товара

--- src/service/cart/CartBuyService.js
+++ src/service/cart/CartBuyService.js
@@ -1,7 +1,7 @@
 // import Notification from "../../components/myBootstrap/Notification"
 import { fetchProductSizes } from "../../http/productAPI"
 
-export const onClickButtonBuy = async (e, product) => {
+export const onClickButtonBuy = async (e, product, type = "add") => {
     e.stopPropagation()
     e.preventDefault()
 
@@ -9,46 +9,65 @@ export const onClickButtonBuy = async (e, product) => {
 
     let cart = localStorage.getItem('cart')
     if (cart) {
-
         cart = JSON.parse(cart)
-        let yes 
-        cart = cart.map(i => {
-            if (i.id === product.id) {
-                yes = "yes"
-                let newValue = i.value + 1
-                return {...i, value: newValue, total: i.price * newValue}
-            } else return i
-        })
-        if (!yes) {
+
+        if (type === "add" || type === "remove") {
+            let yes 
+            cart = cart.map(i => {
+                if (i.id === product.id) {
+                    yes = "yes"
+                    let newValue
+                    if (type === "add") {newValue = i.value + 1}
+                    if (type === "remove") {
+                        if (i.value > 1) {
+                            newValue = i.value - 1
+                        }else {
+                            newValue = i.value
+                        }
+                    }
+                    
+                    return {...i, value: newValue, total: i.price * newValue}
+                } else return i
+            })
+            if (!yes && type === "add") {
+                size = await fetchProductSizes(product.id)
+                cart = [...cart, {
+                    id: product.id,
+                    value: 1,
+                    name: product.name,
+                    article: product.article,
+                    price: product.price,
+                    img: product.img[0].small,
+                    total: product.price,
+                    size
+                }]
+            }
+        }else if (type === "delete") {
+            cart = cart.filter(i => {
+                if (i.id !== product.id) return true
+                return false
+            })
+        }
+        
+    }else {
+
+        if (type === "add") {
             size = await fetchProductSizes(product.id)
-            cart = [...cart, {
-                id: product.id,
-                value: 1,
-                name: product.name,
-                article: product.article,
-                price: product.price,
-                img: product.img[0].small,
+            cart = [{
+                id:product.id,
+                value:1,
+                name:product.name,
+                article:product.article,
+                price:product.price,
+                img:product.img[0].small,
                 total: product.price,
                 size
             }]
         }
-        
-    }else {
-        size = await fetchProductSizes(product.id)
-        cart = [{
-            id:product.id,
-            value:1,
-            name:product.name,
-            article:product.article,
-            price:product.price,
-            img:product.img[0].small,
-            total: product.price,
-            size
-        }]
+
     }
     localStorage.setItem('cart', JSON.stringify(cart))
     // alert("Товар добавлен в корзину.")
 
-    
-    // return <Notification>ghbgfhgfh</Notification>
+    return cart
 }
