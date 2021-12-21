631c459df66d114fd8cea57632566cce62399b94
ogneyar <ogneyar-ne@ya.ru>
2021-12-21 16:19:09 +0300

    добавил в модально окно информацию о товаре с возможностью изменения количества

--- src/components/cart/ButtonBuy.js
+++ src/components/cart/ButtonBuy.js
@@ -3,27 +3,84 @@ import { useHistory } from 'react-router'
 import $ from 'jquery'
 
 import { onClickButtonBuy } from '../../service/cart/CartBuyService'
+// eslint-disable-next-line
+import { CART_ROUTE, CREATE_ORDER_ROUTE, API_URL } from '../../utils/consts'
 import Notification from '../myBootstrap/Notification'
 // eslint-disable-next-line
 import { Button, NavLink } from '../myBootstrap'
-// eslint-disable-next-line
-import { CART_ROUTE, CREATE_ORDER_ROUTE } from '../../utils/consts'
-import './ButtonBuy.css'
+import addDataLayer from '../../service/dataLayer/add'
+import removeDataLayer from '../../service/dataLayer/remove'
 // eslint-disable-next-line
 import CreateOrder from '../order/CreateOrder'
+import './ButtonBuy.css'
 
-import addDataLayer from '../../service/dataLayer/add'
 
 
 const ButtonBuy = (props) => { 
 
     const [notificationVisible, setNotificationVisible] = useState(false)
-
+    
     const history = useHistory()
+
+    const [ value, setValue ] = useState(1) // количество единиц товара
+    const [ quantity, setQuantity ] = useState(1) // общее количество товаров в корзине
 
     let className
     if (props?.className) className = props?.className
 
+    const onClickValue = (e, arg) => {
+        if (arg === "plus") {
+            let cart = onClickButtonBuy(e, props?.product)
+            cart.then(data => {
+                data.forEach(i => {
+                    if (i.id === props?.product?.id) {
+                        setValue(i.value)
+                    }
+                })
+                // setQuantity(data.length)
+            })
+            addDataLayer({
+                article: props?.product?.article,
+                name: props?.product?.name,
+                price: props?.product?.price,
+                value: 1
+            })
+        }
+        if (arg === "minus") {
+            if (value > 1) {
+                let cart = onClickButtonBuy(e, props?.product, "remove")
+                cart.then(data => {
+                    data.forEach(i => {
+                        if (i.id === props?.product?.id) {
+                            setValue(i.value)
+                        }
+                    })
+                    // setQuantity(data.length)
+                })
+                removeDataLayer({
+                    article: props?.product?.article,
+                    name: props?.product?.name,
+                    price: props?.product?.price,
+                    value: 1
+                })
+            }
+        }
+        if (arg === "delete") {
+            let cart = onClickButtonBuy(e, props?.product, "delete")
+            cart.then(data => {
+                setQuantity(data.length)
+                // if (data.length === 0) {
+                    setNotificationVisible(false)
+                // }
+            })
+            removeDataLayer({
+                article: props?.product?.article,
+                name: props?.product?.name,
+                price: props?.product?.price,
+                value
+            })
+        }
+    }
 
     return (
         <>
@@ -32,7 +89,6 @@ const ButtonBuy = (props) => {
             variant="outline-warning"
             onClick={e => {
                 setNotificationVisible(true)
-                onClickButtonBuy(e, props?.product)
                 if (props?.product) {
                     addDataLayer({
                         article: props?.product?.article,
@@ -41,6 +97,16 @@ const ButtonBuy = (props) => {
                         value: 1
                     })
                 }
+                
+                let cart = onClickButtonBuy(e, props?.product)
+                cart.then(data => {
+                    data.forEach(i => {
+                        if (i.id === props?.product?.id) {
+                            setValue(i.value)
+                        }
+                    })
+                    setQuantity(data.length)
+                })
             }}
         >
             {props.children}
@@ -49,40 +115,89 @@ const ButtonBuy = (props) => {
         <Notification 
             show={notificationVisible} 
             onHide={() => setNotificationVisible(false)}
-            className="ButtonBuyNotification"
-            time="10000"
+            time="600000" // в милисекундах
+            size="lg"
+            title="Товар добавлен в корзину!"
+            titleMore={"Товаров в корзине: " + quantity}
         >
             <div
-                className="NotificationDiv"
+                className="ButtonBuyNotification"
             >
-                Товар добавлен в корзину!
-            </div>
+                <div
+                    className="NotificationCart"
+                >
+                    <div
+                        className="NotificationCart_product"
+                    >
+                        <div>
+                            <img src={API_URL + props?.product?.img[0]?.big} width="200" alt="изображение товара" />
+                        </div>
+                        <div>
+                            <div
+                                className="NotificationCart_product_name"
+                            >
+                                {props?.product?.name}
+                            </div>
+                            <br />
+                            <div
+                                className="NotificationCart_product_article"
+                            >
+                                артикул: {props?.product?.article}
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        className="NotificationCart_price"
+                    >
+                        <div>
+                            <label>Цена</label>
+                            {props?.product?.price}
+                        </div>
+                        <div className="NotificationCart_price_value">
+                            <label>Количество</label>
+                            
+                            <small onClick={(e) => onClickValue(e, "minus")} className="NotificationCart_price_value_minus">-</small>
+                            <small className="NotificationCart_price_value_data">{value}</small>
+                            <small onClick={(e) => onClickValue(e, "plus")} className="NotificationCart_price_value_plus">+</small>
+                        </div>
+                        <div>
+                            <label>Итого</label>
+                            {props?.product?.price * value}
+                        </div>
+                        <div>
+                            <label>&nbsp;</label>
+                            <span onClick={(e) => onClickValue(e, "delete")} className="NotificationCart_price_delete"><i className="fa fa-window-close" aria-hidden="true"></i></span>
+                        </div>
+                    </div>
+                </div>
 
-            <div
-                className="NotificationDivButtons"
-            >
-                <Button
-                    variant="outline-success"
-                    onClick={() => {
-                        history.push(CART_ROUTE)
-                        $('html, body').animate(
-                            {
-                                scrollTop: 200
-                            }, 
-                            700, 
-                            function(){}
-                        )
-                    }}
+                <div
+                    className="NotificationDivButtons"
                 >
-                    Перейти к корзине!
-                </Button>
-                
-                <Button
-                    variant="outline-primary"
-                    onClick={() => setNotificationVisible(false)}
-                >
-                    Продолжить покупки
-                </Button>
+                    <Button
+                        // variant="outline-warning"
+                        variant="warning"
+                        onClick={() => {
+                            history.push(CART_ROUTE)
+                            $('html, body').animate(
+                                {
+                                    scrollTop: 200
+                                }, 
+                                700, 
+                                function(){}
+                            )
+                        }}
+                    >
+                        Перейти в корзину!
+                    </Button>
+                    
+                    <Button
+                        variant="outline-primary"
+                        onClick={() => setNotificationVisible(false)}
+                    >
+                        Продолжить покупки
+                    </Button>
+                </div>
             </div>
         </Notification>
         </>
