2d3ab623d1c05bec5c2c203ff8d82dac84b7d401
ogneyar <ogneyar-ne@ya.ru>
2021-12-24 00:03:58 +0300

    добавил номер телефона

--- src/pages/congratulation/CongratulationPage.js
+++ src/pages/congratulation/CongratulationPage.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import ReactHtmlParser from 'react-html-parser'
 import { useQueryParam, NumberParam, StringParam } from 'use-query-params'
 
-import { LK_ROUTE, SUPPORT_ROUTE } from '../../utils/consts'
+import { LK_ROUTE, MAIL, PHONE_ONE, SUPPORT_ROUTE } from '../../utils/consts'
 import NavLink from '../../components/myBootstrap/NavLink'
 import Loading from '../../components/Loading'
 import InfoPage from '../info/InfoPage'
@@ -28,7 +29,7 @@ const CongratulationPage = () => {
 
                 <p>Если Вы зарегистрированный клиент, то посмотреть информацию о Ваших заказах можете в <NavLink to={LK_ROUTE}>личном кабинете</NavLink>, а все возникшие вопросы направляйте в <NavLink to={SUPPORT_ROUTE}>тех. поддержку.</NavLink></p>
 
-                <p>Если Вы НЕ зарегистрированный клиент, тогда все возникающие вопросы направляйте на email адрес - <a href="mailto:it@leidtogi.ru">it@leidtogi.ru</a></p>
+                <p>Если Вы НЕ зарегистрированный клиент, тогда обращайтесть по номеру {ReactHtmlParser(PHONE_ONE)}, либо направляйте возникающие вопросы на email адрес {ReactHtmlParser(MAIL)}</p>
                 
                 <p>Чек с информацией о Вашем заказе будет отправлен на указанный email{email && ` (${email})`}.</p>
 
