751a9632e37a203123d00e6e1510fc284739a78a
ogneyar <ogneyar-ne@ya.ru>
2022-01-20 17:09:06 +0300

    добавил кнопку обновления цен

--- src/pages/parser/ParserPage.js
+++ src/pages/parser/ParserPage.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react'
 import ReactHtmlParser from 'react-html-parser'
 
-import { fetchParserXLSX, fetchParserRGK } from '../../http/paserAPI';
+import { fetchParserXLSX, fetchParserRGK, changePriceOneMilwaukee, changePriceAllMilwaukee } from '../../http/paserAPI';
 import Loading from '../../components/Loading';
 import { observer } from 'mobx-react-lite';
 import InfoPage from '../info/InfoPage';
@@ -23,8 +23,53 @@ const ParserPage = observer(() => {
     const [quantity, setQuantity] = useState(0)
     const [number, setNumber] = useState(0)
     
-    
+
+    const onClickButtonChangePricesMLK = async () => {
+        if ( ! valueBefore || ! valueAfter ) {
+            setMessage("")
+            setLoading(true)
+            await changePriceAllMilwaukee()
+                .then(data => {
+                    if (data?.error) {
+                        setMessage(data.error)
+                    }else {
+                        setMessage(data)
+                    }
+                })
+                .catch(error => {
+                    setMessage(JSON.stringify(error))
+                })
+            setLoading(false)
+            
+            return
+        }
+        setMessage("Начало:")
+        setLoading(true)
+        let mess = "Начало:"
+        for(let i = Number(valueBefore); i <= Number(valueAfter); i++) {
+            await changePriceOneMilwaukee(i)
+            // eslint-disable-next-line
+                .then(data => {
+                    if (data?.error) {
+                        mess += "<br />" + data.error
+                        setMessage(data.error)
+                    }else {
+                        mess += "<br />" + data
+                        setMessage(data)
+                    }
+                })
+                // eslint-disable-next-line
+                .catch(error => {
+                    mess += "<br />" + JSON.stringify(error)
+                    setMessage(JSON.stringify(error))
+                })
+        }
+        setMessage(mess + "<br />Конец.")
+        setLoading(false)
+    }
+
     const onClickButtonParserMLK = async () => {
+        if ( ! valueBefore || ! valueAfter || ! value ) return
         setMessage("")
         setLoading(true)
         let mess = ""
@@ -41,6 +86,11 @@ const ParserPage = observer(() => {
         }
         setLoading(false)
         setMessage(mess + "Закончил.")
+    }
+
+    const onClickButtonChangePricesRGK = async () => {
+        setMessage("")
+        setLoading(true)
     }
     
     const onClickButtonParserRGK = async () => {
@@ -123,7 +173,12 @@ const ParserPage = observer(() => {
                     : null}
 
                     {loading ? <Loading /> 
-                    : <button onClick={onClickButtonParserRGK} >Начать парсинг</button>
+                    : 
+                    <>
+                        <button onClick={onClickButtonParserRGK} >Начать парсинг</button>
+                        <br />
+                        <button onClick={onClickButtonChangePricesRGK}>Обновление цен</button>
+                    </>
                     }
                     <button onClick={() => setBrand("")} >назад</button>
                 </div>
@@ -132,10 +187,9 @@ const ParserPage = observer(() => {
 
     if (brand === "milwaukee")
         return (
-            <div style={{backgroundColor:"white",padding:"50px"}}>
-                <div
-                    className="d-flex flex-column justify-content-center align-items-center"
-                >
+            <InfoPage>
+                <div className="ParserPage_Header">
+                    <label>Заведение товаров Milwaukee на сайт!</label>
                     
                     {message && message !== ""
                     ?
@@ -147,23 +201,27 @@ const ParserPage = observer(() => {
 
                     {loading ? <Loading /> 
                     : 
-                    <div className="inputBox d-flex flex-column justify-content-center align-items-center">
-                        <input className="m-3" value={valueBefore} onChange={(e) => setValueBefore(e.target.value)} placeholder="Введите значение ОТ" />
-                        <input className="m-3" value={valueAfter} onChange={(e) => setValueAfter(e.target.value)} placeholder="Введите значение ДО" />
-                        <input className="m-3" value={value} onChange={(e) => setValue(e.target.value)} placeholder="Введите количество ПО сколько" />
-                        <input className="m-3" value={brand} onChange={(e) => setBrand(e.target.value)} placeholder="Введите бренд" />
-                        
-                        <div
-                            className="d-flex flex-column justify-content-center align-items-center"
-                        >
+                    <>
+                        <div className="inputBox d-flex flex-column justify-content-center align-items-center">
+                            <input className="m-3" value={valueBefore} onChange={(e) => setValueBefore(e.target.value)} placeholder="Введите значение ОТ" />
+                            <input className="m-3" value={valueAfter} onChange={(e) => setValueAfter(e.target.value)} placeholder="Введите значение ДО" />
+                            <input className="m-3" value={value} onChange={(e) => setValue(e.target.value)} placeholder="Введите ПО сколько" />
                             
-                            <button className="m-3 p-2" onClick={onClickButtonParserMLK}>Начать парсинг</button>
+                            <div
+                                className="d-flex flex-column justify-content-center align-items-center"
+                            >
+                                
 
+                                <button className="m-3 p-2" onClick={onClickButtonParserMLK}>Начать парсинг</button>
+
+                            </div>
                         </div>
-                    </div>}
-
-                    <br />
+                        
+                        <br />
+                        <button onClick={onClickButtonChangePricesMLK}>Обновление цен</button>
+                    </>}
                     
+                    <br />
                     <button 
                         className="d-flex flex-column justify-content-center align-items-center p-2"
                         onClick={() => setBrand("")} 
@@ -171,7 +229,7 @@ const ParserPage = observer(() => {
 
                 </div>
 
-            </div>
+            </InfoPage>
         )
 
 })
