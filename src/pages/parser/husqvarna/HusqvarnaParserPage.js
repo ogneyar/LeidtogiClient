145e932e589986f7f3598969b6d9ebbca50fe537
ogneyar <ogneyar-ne@ya.ru>
2022-01-30 22:47:02 +0300

    реализовал возможность добавления новых товаров

--- src/pages/parser/husqvarna/HusqvarnaParserPage.js
+++ src/pages/parser/husqvarna/HusqvarnaParserPage.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import ReactHtmlParser from 'react-html-parser'
 import { observer } from 'mobx-react-lite';
-
-import { getImage, getCharcteristics, getDescription } from '../../../http/parser/husqvarnaAPI'
+// eslint-disable-next-line
+import { addProduct, getLength, addAllProducts, getImage, getCharcteristics, getDescription } from '../../../http/parser/husqvarnaAPI'
 import Loading from '../../../components/Loading';
 import InfoPage from '../../info/InfoPage';
 
@@ -11,54 +11,68 @@ import './HusqvarnaParserPage.css'
 
 const HusqvarnaParserPage = observer((props) => {
 
+    const [feed, setFeed] = useState(null)
+
     const [message, setMessage] = useState("")
     const [loading, setLoading] = useState(false)
+        
+    // const [ article, setArticle ] = useState("9678968-01")
     
-    
-    // const [quantity, setQuantity] = useState(0)
-    // const [number, setNumber] = useState(0)
-    
-    const [ article, setArticle ] = useState("9678968-01")
-    
+    const [ number, setNumber ] = useState(1)
 
     
-    const onClickButtonGetImage = async () => {
+    const onClickButtonAddNewProduct = async () => {
         setMessage("")
-        setLoading(true)
-        if (article !== "") {
-            await getImage(article).then(data => {
-                // console.log("data",data)
-                setMessage(`<img src="${data}" alt="" />`)
-                // setArticle("")
-            })
+        const formData = new FormData()
+        if (feed) {
+            formData.append("feed", feed)
         }
+        setLoading(true)
+        let mess
+        if (number > 0) {
+            await addProduct(formData, number).then(data => {
+                // console.log("data",data)
+                setMessage(data)
+            })
+        }else {
+            let length = await getLength(formData)
+            mess = "Начало:"
+            for(let i = 0; i < length; i++) {
+                await addProduct(formData, i+1)
+                    // eslint-disable-next-line
+                    .then(data => {
+                        if (data?.error) {
+                            mess += "<br />" + data.error
+                            setMessage(data.error)
+                            console.log("data",data.error)
+                        }else {
+                            mess += "<br />" + i + ": " + JSON.stringify(data)
+                            setMessage(i + ": " + JSON.stringify(data))
+                            console.log("data",data)
+                        }
+                    })
+                    // eslint-disable-next-line
+                    .catch(error => {
+                        mess += "<br />" + JSON.stringify(error)
+                        setMessage(JSON.stringify(error))
+                        console.log("error",error)
+                    })
+            }
+            // await addAllProducts(formData).then(data => {
+            //     // console.log("data",data)
+            //     setMessage(data)
+            // })
+        }
+        if (mess) setMessage(mess + "<br />Конец.")
         setLoading(false)
     }
 
-    const onClickButtonGetCharacteristics =async () => {
+    const onClickButtonChangePrices =async () => {
         setMessage("")
         setLoading(true)
-        if (article !== "") {
-            await getCharcteristics(article).then(data => {
-                console.log("data",data)
-                setMessage(data)
-                // setArticle("")
-            })
-        }
-        setLoading(false)
-    }
-
-    const onClickButtonGetDescription =async () => {
-        setMessage("")
-        setLoading(true)
-        if (article !== "") {
-            await getDescription(article).then(data => {
-                // console.log("data",data)
-                setMessage(data)
-                // setArticle("")
-            })
-        }
-        setLoading(false)
+        setTimeout(() => {
+            setLoading(false)
+        }, 1000)
     }
 
 
@@ -87,14 +101,42 @@ const HusqvarnaParserPage = observer((props) => {
                 </>
                 : null}
 
+                <input 
+                    type="file" 
+                    className="m-3" 
+                    onChange={(e) => setFeed(e.target.files[0])} 
+                    placeholder="Выберите файл" 
+                />
+
                 {loading ? <Loading /> 
                 : 
                 <>
-                    <input className="pl-3 pr-3" type="text" value={article} onChange={(e) => setArticle(e.target.value)} />
+                    {/* <input className="pl-3 pr-3" type="text" value={article} onChange={(e) => setArticle(e.target.value)} /> */}
+                    <input 
+                        className="pl-3 pr-3" 
+                        type="text" 
+                        value={number} 
+                        onChange={(e) => setNumber(e.target.value)} 
+                    />
                     <hr />
-                    <button onClick={onClickButtonGetImage} >Гет имэйдж</button>
-                    <button onClick={onClickButtonGetCharacteristics}>Гет чарактеристик</button>
-                    <button onClick={onClickButtonGetDescription}>Гет дескрипшин</button>
+
+                    <button 
+                        // disabled={ ! feed }
+                        className="m-3 p-2" 
+                        onClick={onClickButtonAddNewProduct}
+                    >
+                        Добавить товары
+                    </button>
+                    
+                    <label>или</label>
+                    
+                    <button 
+                        disabled={ ! feed }
+                        onClick={onClickButtonChangePrices}
+                    >
+                        Обновить цены
+                    </button>
+
                     <br />
                 </>
                 }
