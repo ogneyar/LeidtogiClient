8e5ca88c888fe15eab4869ba60d15620c4ce18a5
ogneyar <ogneyar-ne@ya.ru>
2022-01-30 22:45:53 +0300

    добавил новые http запросы

--- src/http/parser/husqvarnaAPI.js
+++ src/http/parser/husqvarnaAPI.js
@@ -2,6 +2,35 @@
 import { $host, $authHost } from '../index'
 
 
+// добавление товара
+export const addProduct = async (formData, number) => {
+    const {data} = await $authHost.post('api/parser/husqvarna/', formData, {
+        params: { 
+            add: true,
+            number
+        }
+    })
+    return data
+}
+
+// возвращает количество всех товаров в файле
+export const getLength = async (formData) => {
+    const {data} = await $authHost.post('api/parser/husqvarna/', formData)
+    return data
+}
+
+// добавление всех товаров из файла
+export const addAllProducts = async (formData) => {
+    const {data} = await $authHost.post('api/parser/husqvarna/', formData, {
+        params: { 
+            add: true,
+            all: true
+        }
+    })
+    return data
+}
+
+
 // получение изображения товара
 export const getImage = async (article) => {
     const {data} = await $host.get('api/parser/husqvarna/get_image', {params: {article}})
