6d4cb1c3a3e7607628d0d21f47e96ee76b2cb920
ogneyar <ogneyar-ne@ya.ru>
2022-02-05 17:55:49 +0300

    добавил функцию changePrices и удалил лишние функции

--- src/http/parser/husqvarnaAPI.js
+++ src/http/parser/husqvarnaAPI.js
@@ -30,21 +30,13 @@ export const addAllProducts = async (formData) => {
     return data
 }
 
-
-// получение изображения товара
-export const getImage = async (article) => {
-    const {data} = await $host.get('api/parser/husqvarna/get_image', {params: {article}})
-    return data
-}
-
-// получение характеристик товара
-export const getCharcteristics = async (article) => {
-    const {data} = await $host.get('api/parser/husqvarna/get_charcteristics', {params: {article}})
-    return data
-}
-
-// получение описания товара
-export const getDescription = async (article) => {
-    const {data} = await $host.get('api/parser/husqvarna/get_description', {params: {article}})
+// смена цен
+export const changePrices = async (formData) => {
+    const {data} = await $authHost.post('api/parser/husqvarna', formData, {
+        params: { 
+            change: true,
+            all: true
+        }
+    })
     return data
 }
