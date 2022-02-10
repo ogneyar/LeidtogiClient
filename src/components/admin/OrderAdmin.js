6f7addabd3880fbddae464eee93e5748fcf86c44
ogneyar <ogneyar-ne@ya.ru>
2022-02-10 18:20:48 +0300

    добавил возможность изменения статуса товара

--- src/components/admin/OrderAdmin.js
+++ src/components/admin/OrderAdmin.js
@@ -1,19 +1,61 @@
-// eslint-disable-next-line
-import React, { useState, useContext } from 'react'
-// eslint-disable-next-line
+import React, { useState, useEffect } from 'react'
 import { Modal, Button } from 'react-bootstrap'
 import { observer } from 'mobx-react-lite'
 
+import { getAllOrders, editOrder } from '../../http/orderAPI'
+import Loading from '../Loading'
+
 
 const OrderAdmin = (props) => {
+
+    const [ loading, setLoading ] = useState(true)
+    const [ error, setError ] = useState("")
+    const [ orders, setOrders ] = useState("")
+
+    useEffect(() => {
+        getAllOrders()
+            .then(data => {
+                if (data.error === undefined) setOrders(data)
+                else setError(data.error)
+            })
+            .catch(err => {
+                setError(err)
+            })
+            .finally(() => setLoading(false))
+    }, []);
+
+    const onClickButtonOrderEnd = async (id) => {
+        setLoading(true)
+        // forming, onway, delivered, taken
+        await editOrder(id, { state: "delivered"})
+            .then(data => {
+                if (data.error === undefined) {
+                    orders.map(i => {
+                        if (i.id === id) return {...i, state: "delivered"}
+                        return i
+                    })
+                    alert("Изменил, обнови страницу...")
+                }else setError(data.error)
+            })
+            .catch(err => {
+                setError(err)
+            })
+            .finally(() => setLoading(false))
+    }
+
+
     return (
         <Modal
+            closeButton
             show={props?.show}
-            // onHide={props?.onHide}
-            onHide={() => null}
-            size="lg"
+            onHide={props?.onHide}
+            // onHide={() => null}
+            // size="sm"
+            // size="lg"
+            width="600"
             aria-labelledby="contained-modal-title-vcenter"
             centered
+            // animation={true}
         >
             <Modal.Header 
                 // closeButton
@@ -24,9 +66,23 @@ const OrderAdmin = (props) => {
             </Modal.Header>
             <Modal.Body>
             
+            {loading ? <Loading />
+            :
+            error ? <div>{error}</div>
+            :
             <div className='d-flex flex-column'>
-                В разработке...
+                {orders && Array.isArray(orders) && orders.map(i => {
+                    return <>
+                        <p>
+                            {i.id}&nbsp;{i.state}&nbsp;{i.pay}&nbsp;
+                            <button
+                                onClick={() => onClickButtonOrderEnd(i.id)}
+                            >Доставлен?</button>
+                        </p>
+                    </>
+                })}
             </div>
+            }
 
             </Modal.Body>
             <Modal.Footer>
