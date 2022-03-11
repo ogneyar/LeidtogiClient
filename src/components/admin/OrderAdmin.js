36c81327e255ea4335e9ae4a8d75c1ba02931dfb
ogneyar <ogneyar-ne@ya.ru>
2022-03-11 14:33:11 +0300

    обновил информацию о заказах

--- src/components/admin/OrderAdmin.js
+++ src/components/admin/OrderAdmin.js
@@ -19,6 +19,7 @@ const OrderAdmin = (props) => {
     const [ loading, setLoading ] = useState(true)
     const [ error, setError ] = useState("")
     const [ orders, setOrders ] = useState("")
+    const [ details, setDetails ] = useState("")
 
     useEffect(() => {
         getAllOrders()
@@ -33,24 +34,31 @@ const OrderAdmin = (props) => {
     }, []);
 
     const onClickButtonOrderEnd = async (id) => {
-        // setLoading(true)
-        // forming, onway, delivered, taken
-        await editOrder(id, { state: "delivered"})
-            .then(data => {
-                if (data.error === undefined) {
-                    setOrders(orders.map(i => {
-                        if (i.id === id) return {...i, state: "delivered"}
-                        return i
-                    }))
-                    // alert("Статус заказа изменён!")
-                    // history.push(ADMIN_ROUTE)
-                    // window.open(ADMIN_ROUTE,'_self',false)
-                }else setError(data.error)
-            })
-            .catch(err => {
-                setError(err)
-            })
-            // .finally(() => setLoading(false))
+        let confirm = window.confirm("Вы уверены что хотите изменить статус заказа №" + id + " на ДОСТАВЛЕН?")
+        if (confirm) {
+            if (user.user.id === 1) {
+            // setLoading(true)
+            // forming, onway, delivered, taken
+            await editOrder(id, { state: "delivered"})
+                .then(data => {
+                    if (data.error === undefined) {
+                        setOrders(orders.map(i => {
+                            if (i.id === id) return {...i, state: "delivered"}
+                            return i
+                        }))
+                        // alert("Статус заказа изменён!")
+                        // history.push(ADMIN_ROUTE)
+                        // window.open(ADMIN_ROUTE,'_self',false)
+                    }else setError(data.error)
+                })
+                .catch(err => {
+                    setError(err)
+                })
+                // .finally(() => setLoading(false))
+            }else {
+                alert("У Вас не достаточно прав доступа, обратитесь к разработчику!")
+            }
+        }
     }
 
     const onClickSetPay = async (id) => {
@@ -103,23 +111,87 @@ const OrderAdmin = (props) => {
             :
             error ? <div>{error}</div>
             :
+            details 
+            ? 
+            <div
+                className="OrderAdmin_details"
+            >
+                <strong>№</strong>&nbsp;{details.id}&nbsp;
+                <br />
+                <strong>Доставка:</strong>&nbsp;{details.delivery}&nbsp;
+                <br />
+                {details.delivery !== "pickup" && 
+                <>
+                    <strong>Адрес:</strong>&nbsp;{details.address}
+                    <br />
+                </>}                
+                {details.phone && 
+                <>
+                    <strong>Телефон:</strong>&nbsp;{details.phone}&nbsp;
+                    <br />
+                </>}
+                {details.name && 
+                <>
+                    <strong>Ф.И.О.:</strong>&nbsp;{details.name}&nbsp;
+                    <br />
+                </>}
+                {details.trackNumber && 
+                <>
+                    <strong>Трек номер:</strong>&nbsp;{details.trackNumber}&nbsp;
+                    <br />
+                </>}                
+                <strong>Корзина:</strong>&nbsp;
+                <br />
+                {JSON.parse(details.cart).map(i => {
+                    return "Артикул: " + i.itemCode + ". Наименование: " + i.name + " - " + i.quantity.value + ` шт. (Цена за штуку - ${i.itemPrice/100}р.) - ` + i.quantity.value * i.itemPrice/100 + "р. | "
+                })}&nbsp;
+                <br />
+                <strong>Email:</strong>&nbsp;{details.email}
+                <br />
+                {details.createdAt && 
+                <>
+                    <strong>Дата создания заявки:</strong>&nbsp;{details.createdAt}&nbsp;
+                    <br />
+                </>}
+                <br /><br />
+                {/* {JSON.stringify(details)} */}
+                {/* <br /><br /> */}
+                <button
+                    className='OrderAdmin_block_button'
+                    onClick={() => setDetails("")}
+                >
+                    Назад
+                </button>
+            </div>
+            :
             <div className='OrderAdmin_block'>
                 {orders && Array.isArray(orders) && orders.map(i => {
                     return <>
                         <p>
-                            {i.id})&nbsp;Статус:&nbsp;{i.state}&nbsp;
-                            {i.pay === 1 ? "Оплачен:  ДА " : <>Оплачен: <span style={{cursor:"pointer"}} onClick={() => onClickSetPay(i.id)}>НЕТ</span></>}&nbsp;
+                            <div
+                                className="OrderAdmin_block_details"
+                                onClick={() => setDetails(i)}
+                                title="Подробное описание заказа"
+                            >
+                                {i.id})&nbsp;Статус:&nbsp;{i.state}&nbsp;
+                            </div>
+                            <br />
+                            {i.pay === 1 ? "Оплачен:  ДА " : <>Оплачен:&nbsp;<span style={{cursor:"pointer"}} onClick={() => onClickSetPay(i.id)} title="Установить статус - оплачен.">НЕТ</span></>}&nbsp;
                             {i.state !== "taken" && i.state !== "delivered" ?
+                            <>
+                                <br /><br />
                                 <button
                                     className='OrderAdmin_block_button'
                                     onClick={() => onClickButtonOrderEnd(i.id)}
                                 >
                                     Доставлен?
                                 </button>
+                            </>
                             : ""}
+                            <hr />
                         </p>
                     </>
-                })}
+                }).reverse()}
             </div>
             }
 
