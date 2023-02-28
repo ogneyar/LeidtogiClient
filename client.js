d766ab0f5722a0554eedf37dbe3fc9606dd3bb7f
ogneyar <ya13th@mail.ru>
2023-02-28 20:38:57 +0300

    eee

---
+++ client.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+
+const port = 3000
+
+const app = express()
+
+app.use(express.static(path.resolve(__dirname,'build','static')))
+app.use(favicon(path.join(__dirname,'build','favicon.ico')))
+
+app.get('/error', (req, res) => res.status(404).sendFile(__dirname + '/build/404.html'))
+
+app.get('*', (req, res) => res.sendFile(__dirname + '/build/index.html'))
+	
+app.listen(port, () => console.log(`Starting server. http://localhost:${ port }`))
