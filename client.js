6b116ddea69d1a14a6a0f55ea79acd47b442c57f
ogneyar <ya13th@mail.ru>
2023-03-02 09:55:04 +0300

    bag fix

--- client.js
+++ client.js
@@ -6,7 +6,7 @@ const port = 3000
 
 const app = express()
 
-app.use(express.static(path.resolve(__dirname,'build','static')))
+app.use(express.static(path.resolve(__dirname,'build')))
 app.use(favicon(path.join(__dirname,'build','favicon.ico')))
 
 app.get('/error', (req, res) => res.status(404).sendFile(__dirname + '/build/404.html'))
