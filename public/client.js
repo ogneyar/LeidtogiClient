69c8b23ca54fe086daaba55bd8dbfaba41e297da
ogneyar <ya13th@mail.ru>
2023-07-02 11:06:31 +0300

    auto commit

--- public/client.js
+++ public/client.js
@@ -1,16 +1,46 @@
+
+// require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const favicon = require('serve-favicon')
 
+const connection = require('./db')
+  
 const port = 3000
+// const PORT = process.env.PORT || 3000
 
 const app = express()
 
 app.use(express.static(path.resolve(__dirname)))
 app.use(favicon(path.join(__dirname,'favicon.ico')))
 
-app.get('/error', (req, res) => res.status(404).sendFile(__dirname + '/404.html'))
+connection.query("SELECT * FROM users WHERE id=1",
+    function(err, results, fields) {
+        if (err) console.log(err)
+        if (results) {
+            // console.log(results) // собственно данные
+            if (false) {
+                app.get('*', (req, res) => res.sendFile(__dirname + '/index.html'))
+                connection.end()
+            }else {
+                
+                // connection.query("SELECT * FROM users WHERE id=1",
+                //     function(err, results, fields) {
+                //         if (results) {
+                //             app.get('*', (req, res) => res.status(404).sendFile(__dirname + '/404.html'))
+                //             connection.end()
+                //         }
+                //     }
+                // )
 
+                // app.get('*', (req, res) => res.status(404).sendFile(__dirname + '/404.html'))
+            }
+        }
+        // console.log(fields) // мета-данные полей 
+    }
+)
+
+app.get('/error', (req, res) => res.status(404).sendFile(__dirname + '/404.html'))
 app.get('*', (req, res) => res.sendFile(__dirname + '/index.html'))
-	
-app.listen(port, () => console.log(`Starting server. http://localhost:${ port }`))
+
+app.listen(port, () => console.log(`Starting server. http://localhost:${port}`))
---
+++ public/db.js
@@ -0,0 +1,10 @@
+
+require('dotenv').config()
+const mysql = require("mysql2")
+
+module.exports = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS
+})
